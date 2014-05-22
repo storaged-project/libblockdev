436514f5dc9e4ba01d547a8e2113d09fbf0a65e9
Vratislav Podzimek <vpodzime@redhat.com>
2014-05-22 14:54:21 +0200

    Use arch-independent format string for guint64 in tests
    Otherwise "0" is printed out even if the right number is returned by the tested
    function.

--- src/lib/test_blockdev.c
+++ src/lib/test_blockdev.c
@@ -1,6 +1,6 @@
 int main (int argc, char* argv[]) {
     bd_init(NULL);
-    g_printf ("max LV size: %lu\n", bd_lvm_get_max_lv_size());
+    g_printf ("max LV size: %"G_GUINT64_FORMAT"\n", bd_lvm_get_max_lv_size());
 
     return 0;
 }
