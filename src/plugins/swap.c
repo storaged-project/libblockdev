aa59708bd72f3b67f39cc014280198f99a7aa17e
Vratislav Podzimek <vpodzime@redhat.com>
2014-06-08 21:08:43 +0200

    Add copyright and doc text for the swap plugin

--- src/plugins/swap.c
+++ src/plugins/swap.c
@@ -1,6 +1,34 @@
+/*
+ * Copyright (C) 2014  Red Hat, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Vratislav Podzimek <vpodzime@redhat.com>
+ */
+
 #include <glib.h>
 #include <string.h>
 #include <unistd.h>
+
+/**
+ * SECTION: swap
+ * @short_description: libblockdev plugin for operations with swap space
+ * @title: Swap
+ * @include: swap.h
+ *
+ * A libblockdev plugin for operations with swap space.
+ */
 
 /**
  * run_and_report_error:
