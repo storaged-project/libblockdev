37c5e9705435e2f23b6949047036504dcd215747
Vratislav Podzimek <vpodzime@redhat.com>
2014-06-11 15:26:39 +0200

    Add missing parameter name in bd_is_plugin_available protype

--- src/lib/blockdev.h
+++ src/lib/blockdev.h
@@ -9,7 +9,7 @@
 gboolean bd_init (BDPluginSpec *force_plugins);
 gboolean bd_reinit (BDPluginSpec *force_plugins, gboolean replace);
 gchar** bd_get_available_plugin_names ();
-gboolean bd_is_plugin_available (BDPlugin);
+gboolean bd_is_plugin_available (BDPlugin plugin);
 
 
 #endif  /* BD_LIB */
