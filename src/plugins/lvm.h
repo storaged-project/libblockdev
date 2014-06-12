8214be49ea94256fb26234dc9971c575f0612dfa
Vratislav Podzimek <vpodzime@redhat.com>
2014-06-12 15:19:47 +0200

    Change pesize to pe_size in function's prototype
    This is how the real argument is called.
    
    Also add blank line to group related functions together.

--- src/plugins/lvm.h
+++ src/plugins/lvm.h
@@ -66,11 +66,12 @@ void bd_lvm_pvdata_free (BDLVMPVdata *data) {
 gboolean bd_lvm_is_supported_pe_size (guint64 size);
 guint64 *bd_lvm_get_supported_pe_sizes ();
 guint64 bd_lvm_get_max_lv_size ();
-guint64 bd_lvm_round_size_to_pe (guint64 size, guint64 pesize, gboolean roundup);
+guint64 bd_lvm_round_size_to_pe (guint64 size, guint64 pe_size, gboolean roundup);
 guint64 bd_lvm_get_lv_physical_size (guint64 lv_size, guint64 pe_size);
 guint64 bd_lvm_get_thpool_padding (guint64 size, guint64 pe_size, gboolean included);
 gboolean bd_lvm_is_valid_thpool_md_size (guint64 size);
 gboolean bd_lvm_is_valid_thpool_chunk_size (guint64 size, gboolean discard);
+
 gboolean bd_lvm_pvcreate (gchar *device, gchar **error_message);
 gboolean bd_lvm_pvresize (gchar *device, guint64 size, gchar **error_message);
 gboolean bd_lvm_pvremove (gchar *device, gchar **error_message);
