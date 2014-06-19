20319b5cee48a209051eece98e5382d345869bbd
Vratislav Podzimek <vpodzime@redhat.com>
2014-06-19 12:57:11 +0200

    Tests for the newly added loop_* functions

--- src/plugins/test_loop.c
+++ src/plugins/test_loop.c
@@ -3,6 +3,7 @@
 int main (int argc, char **argv) {
     gchar *ret = NULL;
     gchar *err_msg = NULL;
+    gboolean success = FALSE;
 
     ret = bd_loop_get_backing_file ("loop0", &err_msg);
     if (!ret)
@@ -10,6 +11,30 @@ int main (int argc, char **argv) {
     else
         g_printf ("loop0's backing device: %s\n", ret);
     g_free (ret);
+    g_free (err_msg);
+
+    ret = bd_loop_get_loop_name ("/tmp/loop_test", &err_msg);
+    if (!ret)
+        g_printf ("Failed to get loop name for /test/loop_test: %s\n", err_msg);
+    else
+        g_printf ("/test/loop_test's loop device: %s\n", ret);
+    g_free (ret);
+    ret = NULL;
+    g_free (err_msg);
+
+    success = bd_loop_setup ("/tmp/loop_test", &ret, &err_msg);
+    if (!success)
+        g_printf ("Failed to setup /tmp/loop_test as loop device: %s", err_msg);
+    else
+        g_printf ("/tmp/loop_test setup at %s", ret);
+    g_free (ret);
+    g_free (err_msg);
+
+    success = bd_loop_teardown ("/dev/loop0", &err_msg);
+    if (!success)
+        g_printf ("Failed to teardown /dev/loop0: %s", err_msg);
+    else
+        puts ("/dev/loop0 torn down");
     g_free (err_msg);
 
     return 0;
