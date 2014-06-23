291cd9a6400e7469fefaf8d05f604f15f0f46f41
Vratislav Podzimek <vpodzime@redhat.com>
2014-06-23 08:38:32 +0200

    Add tests for the no-device LVM functions
    Some functions from the LVM plugin don't operate on any device and should thus
    be put into a separate TestCase.

---
+++ tests/lvm_test.py
@@ -0,0 +1,118 @@
+import unittest
+import os
+import math
+
+from utils import create_sparse_tempfile
+from gi.repository import BlockDev
+BlockDev.init(None)
+
+class LvmNoDevTestCase(unittest.TestCase):
+    def test_is_supported_pe_size(self):
+        """Verify that lvm_is_supported_pe_size works as expected"""
+
+        self.assertTrue(BlockDev.lvm_is_supported_pe_size(4 * 1024))
+        self.assertTrue(BlockDev.lvm_is_supported_pe_size(4 * 1024**2))
+        self.assertTrue(BlockDev.lvm_is_supported_pe_size(6 * 1024**2))
+        self.assertTrue(BlockDev.lvm_is_supported_pe_size(12 * 1024**2))
+        self.assertTrue(BlockDev.lvm_is_supported_pe_size(15 * 1024**2))
+        self.assertTrue(BlockDev.lvm_is_supported_pe_size(4 * 1024**3))
+
+        self.assertFalse(BlockDev.lvm_is_supported_pe_size(512))
+        self.assertFalse(BlockDev.lvm_is_supported_pe_size(4097))
+        self.assertFalse(BlockDev.lvm_is_supported_pe_size(65535))
+        self.assertFalse(BlockDev.lvm_is_supported_pe_size(32 * 1024**3))
+
+    def test_get_supported_pe_sizes(self):
+        """Verify that supported PE sizes are really supported"""
+
+        for size in BlockDev.lvm_get_supported_pe_sizes():
+            self.assertTrue(BlockDev.lvm_is_supported_pe_size(size))
+
+    def test_get_max_lv_size(self):
+        """Verify that max LV size is correctly determined"""
+
+        if os.uname()[-1] == "i686":
+            # 32-bit arch
+            expected = 16 * 1024**4
+        else:
+            # 64-bit arch
+            expected = 8 * 1024**5
+
+        self.assertEqual(BlockDev.lvm_get_max_lv_size(), expected)
+
+    def test_round_size_to_pe(self):
+        """Verify that round_size_to_pe works as expected"""
+
+        self.assertEqual(BlockDev.lvm_round_size_to_pe(11 * 1024**2, 4 * 1024**2, True), 12 * 1024**2)
+        self.assertEqual(BlockDev.lvm_round_size_to_pe(11 * 1024**2, 4 * 1024**2, False), 8 * 1024**2)
+
+        # default PE size is 4 MiB
+        self.assertEqual(BlockDev.lvm_round_size_to_pe(11 * 1024**2, 0, True), 12 * 1024**2)
+        self.assertEqual(BlockDev.lvm_round_size_to_pe(11 * 1024**2, 0, False), 8 * 1024**2)
+
+    def test_get_lv_physical_size(self):
+        """Verify that get_lv_physical_size works as expected"""
+
+        self.assertEqual(BlockDev.lvm_get_lv_physical_size(25 * 1024**3, 4 * 1024**2),
+                         25 * 1024**3 + 4 * 1024**2)
+
+        # default PE size is 4 MiB
+        self.assertEqual(BlockDev.lvm_get_lv_physical_size(25 * 1024**3, 0),
+                         25 * 1024**3 + 4 * 1024**2)
+
+        self.assertEqual(BlockDev.lvm_get_lv_physical_size(11 * 1024**2, 4 * 1024**2),
+                         16 * 1024**2)
+
+    def test_get_thpool_padding(self):
+        """Verify that get_thpool_padding works as expected"""
+
+        expected_padding = BlockDev.lvm_round_size_to_pe(int(math.ceil(11 * 1024**2 * 0.2)),
+                                                         4 * 1024**2, True)
+        self.assertEqual(BlockDev.lvm_get_thpool_padding(11 * 1024**2, 4 * 1024**2, False),
+                         expected_padding)
+
+        expected_padding = BlockDev.lvm_round_size_to_pe(int(math.ceil(11 * 1024**2 * (1.0/6.0))),
+                                                         4 * 1024**2, True)
+        self.assertEqual(BlockDev.lvm_get_thpool_padding(11 * 1024**2, 4 * 1024**2, True),
+                         expected_padding)
+
+    def test_is_valid_thpool_md_size(self):
+        """Verify that is_valid_thpool_md_size works as expected"""
+
+        self.assertTrue(BlockDev.lvm_is_valid_thpool_md_size(2 * 1024**2))
+        self.assertTrue(BlockDev.lvm_is_valid_thpool_md_size(3 * 1024**2))
+        self.assertTrue(BlockDev.lvm_is_valid_thpool_md_size(16 * 1024**3))
+
+        self.assertFalse(BlockDev.lvm_is_valid_thpool_md_size(1 * 1024**2))
+        self.assertFalse(BlockDev.lvm_is_valid_thpool_md_size(17 * 1024**3))
+
+    def test_is_valid_thpool_chunk_size(self):
+        """Verify that is_valid_thpool_chunk_size works as expected"""
+
+        # 64 KiB is OK with or without discard
+        self.assertTrue(BlockDev.lvm_is_valid_thpool_chunk_size(64 * 1024, True))
+        self.assertTrue(BlockDev.lvm_is_valid_thpool_chunk_size(64 * 1024, False))
+
+        # 192 KiB is OK without discard, but NOK with discard
+        self.assertTrue(BlockDev.lvm_is_valid_thpool_chunk_size(192 * 1024, False))
+        self.assertFalse(BlockDev.lvm_is_valid_thpool_chunk_size(192 * 1024, True))
+
+        # 191 KiB is NOK in both cases
+        self.assertFalse(BlockDev.lvm_is_valid_thpool_chunk_size(191 * 1024, False))
+        self.assertFalse(BlockDev.lvm_is_valid_thpool_chunk_size(191 * 1024, True))
+
+
+class LvmTestCase(unittest.TestCase):
+    def setUp(self):
+        self.dev_file = create_sparse_tempfile("libblockdev_lvm_test", 1024**3)
+        succ, loop, err = BlockDev.loop_setup(self.dev_file)
+        if err or not succ:
+            raise RuntimeError("Failed to setup loop device for testing")
+        self.loop_dev = "/dev/%s" % loop
+
+    def tearDown(self):
+        succ, err = BlockDev.loop_teardown(self.loop_dev)
+        if err or not succ:
+            os.unlink(self.dev_file)
+            raise RuntimeError("Failed to tear down loop device used for testing")
+
