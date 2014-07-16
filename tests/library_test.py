0454424b2dd0e8d817b976a0f927c2c007db0042
Vratislav Podzimek <vpodzime@redhat.com>
2014-07-16 17:35:50 +0200

    Test for forcing specific plugin(s)

--- tests/library_test.py
+++ tests/library_test.py
@@ -37,3 +37,42 @@ class LibraryOpsTestCase(unittest.TestCase):
         # library should successfully reinitialize reloading original plugins
         self.assertTrue(BlockDev.reinit(None, True))
 
+    def test_force_plugin(self):
+        """Verify that forcing plugin to be used works as expected"""
+
+        # library should successfully initialize
+        self.assertTrue(BlockDev.init(None))
+
+        # max LV size should be something sane (not 1024 bytes)
+        orig_max_size = BlockDev.lvm_get_max_lv_size()
+        self.assertNotEqual(orig_max_size, 1024)
+
+        # change the sources and recompile
+        os.system("sed -ri 's?MAX_LV_SIZE;?1024;//test-change?' src/plugins/lvm.c > /dev/null")
+        os.system("make src/plugins/libbd_lvm.so > /dev/null")
+
+        # proclaim the new build a different plugin
+        os.system("cp src/plugins/libbd_lvm.so src/plugins/libbd_lvm2.so")
+
+        # change the sources back and recompile
+        os.system("sed -ri 's?1024;//test-change?MAX_LV_SIZE;?' src/plugins/lvm.c > /dev/null")
+        os.system("make src/plugins/libbd_lvm.so > /dev/null")
+
+        # force the new plugin to be used
+        ps = BlockDev.PluginSpec()
+        ps.name = BlockDev.Plugin.LVM
+        ps.so_name = "libbd_lvm2.so"
+        self.assertTrue(BlockDev.reinit([ps], True))
+
+        # new LVM plugin loaded, max LV size should be 1024 bytes
+        self.assertEqual(BlockDev.lvm_get_max_lv_size(), 1024)
+
+        # clean after ourselves
+        os.system ("rm -f src/plugins/libbd_lvm2.so")
+
+        # force the old plugin to be used
+        ps = BlockDev.PluginSpec()
+        ps.name = BlockDev.Plugin.LVM
+        ps.so_name = "libbd_lvm.so"
+        self.assertTrue(BlockDev.reinit([ps], True))
+
