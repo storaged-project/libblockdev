cd00b1fe650e934955612bc81ce55f8e5ce01bb7
Vratislav Podzimek <vpodzime@redhat.com>
2015-01-26 15:55:07 +0100

    Fix parameters in luks_open python overrides
    The 'name' parameter was missing.

--- src/python/BlockDev.py
+++ src/python/BlockDev.py
@@ -45,8 +45,8 @@ __all__.append("crypto_luks_format")
 
 _crypto_luks_open = BlockDev.crypto_luks_open
 @override(BlockDev.crypto_luks_open)
-def crypto_luks_open(device, passphrase=None, key_file=None):
-    return _crypto_luks_open(device, passphrase, key_file)
+def crypto_luks_open(device, name, passphrase=None, key_file=None):
+    return _crypto_luks_open(device, name, passphrase, key_file)
 __all__.append("crypto_luks_open")
 
 _crypto_luks_add_key = BlockDev.crypto_luks_add_key
