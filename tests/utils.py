7556def7c9d591b9579742545bb2f40777b26ed8
Vratislav Podzimek <vpodzime@redhat.com>
2015-04-08 15:50:10 +0200

    Fix default value for the fake_utils' path argument
    The default should be a valid string that can be prepended to $PATH, so "." is
    much better than "".

--- tests/utils.py
+++ tests/utils.py
@@ -31,7 +31,7 @@ def udev_settle():
     os.system("udevadm settle")
 
 @contextmanager
-def fake_utils(path=""):
+def fake_utils(path="."):
     old_path = os.environ.get("PATH", "")
     if old_path:
         new_path = path + ":" + old_path
