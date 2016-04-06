b7174f8c0d23474aa679ed29c95c4377fe016fd2
Vratislav Podzimek <vpodzime@redhat.com>
2016-04-06 12:08:22 +0200

    Adapt to another stupid change in btrfs-progs
    It must have been like this:
    
    Why not replace "can't" with "cannot"? It looks better, doesn't it?! And who
    cares the CLI output is actually our API?

--- tests/btrfs_test.py
+++ tests/btrfs_test.py
@@ -247,7 +247,7 @@ class BtrfsTestGetDefaultSubvolumeID(BtrfsTestCase):
         self.assertTrue(succ)
 
         # not mounted yet, should fail
-        with six.assertRaisesRegex(self, GLib.GError, r".*can't access.*"):
+        with six.assertRaisesRegex(self, GLib.GError, r".*(can't|cannot) access.*"):
             ret = BlockDev.btrfs_get_default_subvolume_id(TEST_MNT)
 
         mount(self.loop_dev, TEST_MNT)
