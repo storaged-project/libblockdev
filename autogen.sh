9655123be9a34f08ba2998b6eaf13b0919681c43
Vratislav Podzimek <vpodzime@redhat.com>
2017-07-10 11:38:29 +0200

    Remove old code from the gh-pages branch
    It somehow slipped in here long time ago.

--- autogen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-[ -d m4 ] || mkdir m4
-libtoolize --copy --force
-aclocal -I m4
-autoconf
-automake --foreign --add-missing --copy
-rm -rf autom4te.cache
--- tests/__init__.py
+++
@@ -1,2 +0,0 @@
-import gi.overrides
-gi.overrides.__path__.insert(0, "src/python")
