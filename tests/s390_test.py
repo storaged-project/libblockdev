05e4b5690166ce87e56ca7598738103c80b68ae3
Vojtech Trefny <vtrefny@redhat.com>
2017-09-07 13:31:13 +0200

    Bypass error proxy in s390 test

--- tests/s390_test.py
+++ tests/s390_test.py
@@ -21,61 +21,61 @@ class S390TestCase(unittest.TestCase):
     def test_device_input(self):
         """Verify that s390_sanitize_dev_input works as expected"""
         dev = "1234"
-        self.assertEqual(BlockDev.s390.sanitize_dev_input(dev), '0.0.' + dev)
+        self.assertEqual(BlockDev.s390_sanitize_dev_input(dev), '0.0.' + dev)
 
         dev = "123456"
-        self.assertEqual(BlockDev.s390.sanitize_dev_input(dev), '0.0.' + dev)
+        self.assertEqual(BlockDev.s390_sanitize_dev_input(dev), '0.0.' + dev)
 
         # the device number is padded on the left with 0s up to 4 digits
         dev = "123.abc"
-        self.assertEqual(BlockDev.s390.sanitize_dev_input(dev), "0.0.0abc")
+        self.assertEqual(BlockDev.s390_sanitize_dev_input(dev), "0.0.0abc")
         dev = "abc"
-        self.assertEqual(BlockDev.s390.sanitize_dev_input(dev), "0.0.0abc")
+        self.assertEqual(BlockDev.s390_sanitize_dev_input(dev), "0.0.0abc")
         dev = ".abc"
-        self.assertEqual(BlockDev.s390.sanitize_dev_input(dev), "0.0.0abc")
+        self.assertEqual(BlockDev.s390_sanitize_dev_input(dev), "0.0.0abc")
 
         # a complete number is unchanged
         dev = "0.0.abcd"
-        self.assertEqual(BlockDev.s390.sanitize_dev_input(dev), dev)
+        self.assertEqual(BlockDev.s390_sanitize_dev_input(dev), dev)
 
         # a too long number doesn't mean a crash no matter if it makes sense
         dev = "0.0.abcdefgh"
-        self.assertEqual(BlockDev.s390.sanitize_dev_input(dev), dev)
+        self.assertEqual(BlockDev.s390_sanitize_dev_input(dev), dev)
 
     def test_wwpn_input(self):
         """Verify that s390_zfcp_sanitize_wwpn_input works as expected"""
         # missing "0x" from beginning of wwpn; this should be added by fx
         wwpn = "01234567abcdefab"
-        self.assertEqual(BlockDev.s390.zfcp_sanitize_wwpn_input(wwpn), "0x01234567abcdefab")
+        self.assertEqual(BlockDev.s390_zfcp_sanitize_wwpn_input(wwpn), "0x01234567abcdefab")
         # this should be fine as-is
         wwpn = "0x01234567abcdefab"
-        self.assertEqual(BlockDev.s390.zfcp_sanitize_wwpn_input(wwpn), wwpn)
+        self.assertEqual(BlockDev.s390_zfcp_sanitize_wwpn_input(wwpn), wwpn)
 
         # too short
         wwpn = "a"
         with self.assertRaises(GLib.GError):
-            BlockDev.s390.zfcp_sanitize_wwpn_input(wwpn)
+            BlockDev.s390_zfcp_sanitize_wwpn_input(wwpn)
 
     def test_lun_input(self):
         """Verify that s390_zfcp_sanitize_lun_input works as expected"""
         # user does not prepend lun with "0x"; this should get added
         lun = "01234567abcdefab"
-        self.assertEqual(BlockDev.s390.zfcp_sanitize_lun_input(lun), "0x01234567abcdefab")
+        self.assertEqual(BlockDev.s390_zfcp_sanitize_lun_input(lun), "0x01234567abcdefab")
         # a user enters a lun that is between 0 and 16 chars long (non-inclusive); 0 padding should be added to expand to 16
         lun = "0x123"
-        self.assertEqual(BlockDev.s390.zfcp_sanitize_lun_input(lun), "0x0123000000000000")
+        self.assertEqual(BlockDev.s390_zfcp_sanitize_lun_input(lun), "0x0123000000000000")
         lun = "0x12345"
-        self.assertEqual(BlockDev.s390.zfcp_sanitize_lun_input(lun), "0x1234500000000000")
+        self.assertEqual(BlockDev.s390_zfcp_sanitize_lun_input(lun), "0x1234500000000000")
         lun = "0x123456"
-        self.assertEqual(BlockDev.s390.zfcp_sanitize_lun_input(lun), "0x1234560000000000")
+        self.assertEqual(BlockDev.s390_zfcp_sanitize_lun_input(lun), "0x1234560000000000")
         # this should be fine as-is
         lun = "0x1234567800000000"
-        self.assertEqual(BlockDev.s390.zfcp_sanitize_lun_input(lun), lun)
+        self.assertEqual(BlockDev.s390_zfcp_sanitize_lun_input(lun), lun)
 
         # too long
         lun = "12345678901234567890"
         with self.assertRaises(GLib.GError):
-            BlockDev.s390.zfcp_sanitize_lun_input(lun)
+            BlockDev.s390_zfcp_sanitize_lun_input(lun)
 
 
 @unittest.skipUnless(os.uname()[4].startswith('s390'), "s390x architecture required")
