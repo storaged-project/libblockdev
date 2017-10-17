c112efb223c6dcdc1b42975d8cc4bc2ce56a3fec
Vojtech Trefny <vtrefny@redhat.com>
2017-10-17 13:04:45 +0200

    fs.c: Check sscanf return value in 'bd_fs_vfat_get_info'

--- src/plugins/fs.c
+++ src/plugins/fs.c
@@ -2780,6 +2780,7 @@ BDFSVfatInfo* bd_fs_vfat_get_info (const gchar *device, GError **error) {
     guint64 full_cluster_count = 0;
     guint64 cluster_count = 0;
     gchar **key_val = NULL;
+    gint scanned = 0;
 
     if (!check_deps (&avail_deps, DEPS_FSCKVFAT_MASK, deps, DEPS_LAST, &deps_check_lock, error))
         return FALSE;
@@ -2865,8 +2866,13 @@ BDFSVfatInfo* bd_fs_vfat_get_info (const gchar *device, GError **error) {
             have_cluster_size = TRUE;
         } else if (!have_cluster_count && g_str_has_prefix (*line_p, device)) {
             key_val = g_strsplit (*line_p, ",", 2);
-            sscanf (key_val[1], " %" G_GUINT64_FORMAT "/" "%" G_GUINT64_FORMAT " clusters",
-                    &full_cluster_count, &cluster_count);
+            scanned = sscanf (key_val[1], " %" G_GUINT64_FORMAT "/" "%" G_GUINT64_FORMAT " clusters",
+                              &full_cluster_count, &cluster_count);
+            if (scanned != 2) {
+                g_set_error (error, BD_FS_ERROR, BD_FS_ERROR_FAIL,
+                             "Failed to get number of FAT clusters for '%s'", device);
+                return FALSE;
+            }
             ret->cluster_count = cluster_count;
             ret->free_cluster_count = cluster_count - full_cluster_count;
             have_cluster_count = TRUE;
