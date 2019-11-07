4db1941c8ed05b1e83a5370ee061b6b951e05ad9
Vojtech Trefny <vtrefny@redhat.com>
2019-11-07 08:38:13 +0100

    Add new function 'bd_fs_wipe_force' to control force wipe
    Our current implementation allows to wipe mounted devices by
    default. We can't change this behaviour but we want to allow to
    control this so we need a new function.

--- src/plugins/fs/generic.h
+++ src/plugins/fs/generic.h
@@ -4,6 +4,7 @@
 #define BD_FS_GENERIC
 
 gboolean bd_fs_wipe (const gchar *device, gboolean all, GError **error);
+gboolean bd_fs_wipe_force (const gchar *device, gboolean all, gboolean force, GError **error);
 gboolean bd_fs_clean (const gchar *device, GError **error);
 gchar* bd_fs_get_fstype (const gchar *device,  GError **error);
 
