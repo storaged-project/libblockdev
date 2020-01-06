5d15b4253e4a35f3d9fc8ea0e94d8d11b006d7d3
Vojtech Trefny <vtrefny@redhat.com>
2020-01-06 15:56:14 +0100

    Fix getting string representation of unknown VDO state index

--- src/plugins/lvm-dbus.c
+++ src/plugins/lvm-dbus.c
@@ -3660,6 +3660,8 @@ const gchar* bd_lvm_get_vdo_index_state_str (BDLVMVDOIndexState state, GError **
         return "offline";
     case BD_LVM_VDO_INDEX_ONLINE:
         return "online";
+    case BD_LVM_VDO_INDEX_UNKNOWN:
+        return "unknown";
     default:
         g_set_error (error, BD_LVM_ERROR, BD_LVM_ERROR_FAIL,
                      "Invalid LVM VDO index state.");
