62765a32e551bef2c319958245232d9fe0b7db35
Vojtech Trefny <vtrefny@redhat.com>
2020-09-25 14:26:57 +0200

    mdraid: Fix copy-paste error when checking return value

--- src/plugins/mdraid.c
+++ src/plugins/mdraid.c
@@ -995,7 +995,7 @@ BDMDExamineData* bd_md_examine (const gchar *device, GError **error) {
     orig_data = ret->dev_uuid;
     if (orig_data) {
         ret->dev_uuid = bd_md_canonicalize_uuid (orig_data, error);
-        if (!ret->uuid) {
+        if (!ret->dev_uuid) {
             g_prefix_error (error, "Failed to canonicalize MD UUID '%s': ", orig_data);
             g_free (orig_data);
             bd_md_examine_data_free (ret);
